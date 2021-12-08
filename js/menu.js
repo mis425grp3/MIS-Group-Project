e2d0952b0a334f1511354ff1318e5c0cb534c786
aebange <95502557+mis425grp3@users.noreply.github.com>
2021-12-08 19:08:24 +0000

    Code cleanup

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,36 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-  document.getElementById('pizza1').addEventListener('mouseover',function(){
-    document.getElementById('pizza1').classList.add('hovercss');
-  })
+    document.getElementById('pizza1').addEventListener('mouseover', function () {
+        document.getElementById('pizza1').classList.add('hovercss');
+    })
 
-  document.getElementById('pizza1').addEventListener('mouseout', function(){
-    document.getElementById('pizza1').classList.remove('hovercss');
-  });
+    document.getElementById('pizza1').addEventListener('mouseout', function () {
+        document.getElementById('pizza1').classList.remove('hovercss');
+    });
 
-  document.getElementById('pizza2').addEventListener('mouseover',function(){
-    document.getElementById('pizza2').classList.add('hovercss');
-  })
+    document.getElementById('pizza2').addEventListener('mouseover', function () {
+        document.getElementById('pizza2').classList.add('hovercss');
+    })
 
-  document.getElementById('pizza2').addEventListener('mouseout', function(){
-    document.getElementById('pizza2').classList.remove('hovercss');
-  });
+    document.getElementById('pizza2').addEventListener('mouseout', function () {
+        document.getElementById('pizza2').classList.remove('hovercss');
+    });
 
-  document.getElementById('pizza3').addEventListener('mouseover',function(){
-    document.getElementById('pizza3').classList.add('hovercss');
-  })
+    document.getElementById('pizza3').addEventListener('mouseover', function () {
+        document.getElementById('pizza3').classList.add('hovercss');
+    })
 
-  document.getElementById('pizza3').addEventListener('mouseout', function(){
-    document.getElementById('pizza3').classList.remove('hovercss');
-  });
+    document.getElementById('pizza3').addEventListener('mouseout', function () {
+        document.getElementById('pizza3').classList.remove('hovercss');
+    });
 
-  document.getElementById('pizza4').addEventListener('mouseover',function(){
-    document.getElementById('pizza4').classList.add('hovercss');
-  })
+    document.getElementById('pizza4').addEventListener('mouseover', function () {
+        document.getElementById('pizza4').classList.add('hovercss');
+    })
 
-  document.getElementById('pizza4').addEventListener('mouseout', function(){
-    document.getElementById('pizza4').classList.remove('hovercss');
-  });
+    document.getElementById('pizza4').addEventListener('mouseout', function () {
+        document.getElementById('pizza4').classList.remove('hovercss');
+    });
 
-  
+
 });
