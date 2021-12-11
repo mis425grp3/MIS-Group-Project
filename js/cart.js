a82c965e1a824a2fef3e867c2674161912abf6f7
aebange <95502557+mis425grp3@users.noreply.github.com>
2021-12-11 04:07:54 +0000

    Mixed Changes

--- js/cart.js
+++ js/cart.js
@@ -33,11 +33,24 @@ for (const listItem of cartContentsObjArray) {
         listItemSum = (12.75*parseInt(listItemQty)).toFixed(2)
     } else {
         // Determine the name
-        listItemName = listItem.pizzaType;
-        listItemToppings = "N/A - Specialty Pizza"
-        listItemQty = listItem.itemQty;
-        listItemPrice = "12.75"
-        listItemSum = (12.75*parseInt(listItemQty)).toFixed(2)
+        listItemName = "Custom w/" + listItem['sauceType'];
+        // Get a string containing every topping
+        let toppingString = "";
+        let toppingDict = ["Pepperoni", "Sausage", "Meatballs", "Mushroom", "Spinach"]
+        let topperDict = ["Hot Sauce Drizzle", "Basil Leaves", "Oregano"]
+        for ([index, toppingItem] of listItem['toppingArray'].entries())
+            if (toppingItem === true) {
+                toppingString += toppingDict[index] + ', '
+            }
+        for ([index, toppingItem] of listItem['topperArray'].entries())
+            if (toppingItem === true) {
+                toppingString += topperDict[index] + ', '
+            }
+        toppingString = toppingString.substring(0 ,toppingString.length -1);
+        listItemToppings = toppingString;
+        listItemQty = listItem['itemQty'];
+        listItemPrice = "13.75"
+        listItemSum = (13.75*parseInt(listItemQty)).toFixed(2)
     }
 
     // let listItemProductImg = './images/blankPizza.png';
@@ -64,9 +77,15 @@ for (const listItem of cartContentsObjArray) {
         "                    <td>\n" +
         "                        <p>" + listItemToppings + "</p>\n" +
         "                    </td>\n" +
-        "                    <td>$ <span id=\"quantity\" class=\"qty\">"+listItemQty+"</span></td>\n" +
+        "                    <td><span id=\"quantity\" class=\"qty\">"+listItemQty+"</span></td>\n" +
         "                    <td>$ <span id=\"price\" class=\"price\">"+listItemPrice+"</span></td>\n" +
         "                    <td>$ <span id=\"sum\" class=\"sum\">"+listItemSum+"</span></td>\n" +
         "                </tr>"
     console.log("Inserted row?")
 }
+
+// let ??? = [
+//   {"Buffalo Chicken Pizza": "images/blPizza.png", 
+//   "Meat Lover's Pizza":"images/mlPizza.png",
+//   "Baked Ziti Pizza":"images/bzPizza.png","Chicken Parmesan Pizza":"images/cpPizza.png", "Build Your Own":"images/byoPizza.png"}
+// ]
