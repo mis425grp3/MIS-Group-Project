21ea62fc878fb0743d37848cf454e729c7371952
aebange <95502557+mis425grp3@users.noreply.github.com>
2021-12-11 00:13:31 +0000

    Mixed changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Handle the sessionStorage
 let cartObj = undefined;
-while (cartObj == undefined) {
+while (
+    cartObj === undefined) {
     cartObj = sessionStorage.getItem('papaGCart');
-    if (cartObj == undefined) {
+    if (cartObj === undefined) {
         console.log("New browser session, creating cart parent object")
         // The cartObj didn't exist, must be a new instance - create a new parent obj
         let papaGCart = {
@@ -26,38 +27,82 @@ addToCartFormNode.addEventListener('submit', function(e) {
     let formSpecialtyPizzaQty = document.getElementById("qty").value;
 
     // Get user input on sauce selections for the pizza building option
-    let formCustomPizzaTSauceBool = document.getElementById("tSauce").value;
-    let formCustomPizzaBSauceBool = document.getElementById("bSauce").value;
-    let formCustomPizzaWSauceBool = document.getElementById("wSauce").value;
-    let formCustomSauceArray = [formCustomPizzaTSauceBool, formCustomPizzaBSauceBool, formCustomPizzaWSauceBool];
+    let formCustomPizzaTSauceBool = document.getElementById("tSauce").checked;
+    let formCustomPizzaBSauceBool = document.getElementById("bSauce").checked;
+    let formCustomPizzaWSauceBool = document.getElementById("wSauce").checked;
+    const formCustomSauceArray = [formCustomPizzaTSauceBool, formCustomPizzaBSauceBool, formCustomPizzaWSauceBool];
     let formCustomSauceSelection = "";
 
     // Get user input on topping selections
-    let formCustomPizzaPepTopBool = document.getElementById('pepperoni').value;
-    let formCustomPizzaSauTopBool = document.getElementById('sausage').value;
-    let formCustomPizzaMeaTopBool = document.getElementById('meatballs').value;
-    let formCustomPizzaMusTopBool = document.getElementById('mushroom').value;
-    let formCustomPizzaSpiTopBool = document.getElementById('spinach').value;
-    let formCustomToppingArray = [formCustomPizzaPepTopBool, formCustomPizzaSauTopBool, formCustomPizzaMeaTopBool, formCustomPizzaMusTopBool, formCustomPizzaSpiTopBool];
+    let formCustomPizzaPepTopBool = document.getElementById('pepperoni').checked;
+    let formCustomPizzaSauTopBool = document.getElementById('sausage').checked;
+    let formCustomPizzaMeaTopBool = document.getElementById('meatballs').checked;
+    let formCustomPizzaMusTopBool = document.getElementById('mushroom').checked;
+    let formCustomPizzaSpiTopBool = document.getElementById('spinach').checked;
+    const formCustomToppingArray = [formCustomPizzaPepTopBool, formCustomPizzaSauTopBool, formCustomPizzaMeaTopBool, formCustomPizzaMusTopBool, formCustomPizzaSpiTopBool];
 
     // Get user input on topper selections
-    let formCustomPizzaHotTprBool = document.getElementById('hotSauce').value;
-    let formCustomPizzaBasTprBool = document.getElementById('basil').value;
-    let formCustomPizzaOreTprBool = document.getElementById('oregano').value;
-    let formCustomTopperArray = [formCustomPizzaHotTprBool, formCustomPizzaBasTprBool, formCustomPizzaOreTprBool];
+    let formCustomPizzaHotTprBool = document.getElementById('hotSauce').checked;
+    let formCustomPizzaBasTprBool = document.getElementById('basil').checked;
+    let formCustomPizzaOreTprBool = document.getElementById('oregano').checked;
+    const formCustomTopperArray = [formCustomPizzaHotTprBool, formCustomPizzaBasTprBool, formCustomPizzaOreTprBool];
 
     // Get user input on custom pizza quantity
     let formCustomPizzaQty = document.getElementById('qty_custom').value;
 
     // TODO: Data Validation
-    let submissionType = 'specialty';
-    // if (formSpecialtyPizza != undefined || formSpecialtyPizzaQty != undefined) {
-    //
-    // }
+    // Check to see if anything was entered into the custom fields
+    let customState = false;
+    let customArrayArray = [formCustomSauceArray, formCustomToppingArray, formCustomTopperArray];
+    for (const formArray of customArrayArray) {
+        for (const itemBool of formArray) {
+            if (itemBool === true) {
+                customState = true
+                break
+            }
+        }
+        if (customState === true) {
+            break
+        }
+    }
+    if (formCustomPizzaQty !== "") {
+        customState = true;
+    }
+
+    if (formSpecialtyPizza !== "" || formSpecialtyPizzaQty !== "") {
+        // One of these fields was populated, so let's assume this is what they were trying to add
+        let submissionType = 'specialty';
+        // Make sure we've got everything we need
+        if (formSpecialtyPizza === "") {
+            alert('Please select a type of specialty pizza.')
+            return
+        } else if (formSpecialtyPizzaQty === "") {
+            alert('Please select a specialty pizza quantity.')
+            return
+        }
+    } else if (customState === true) {
+        // One of these fields was populated, so let's assume this is what they were trying to add
+        let submissionType = 'custom';
+        // Make sure we've got everything we need
+        let sauceSelectionState = false
+        for (item of formCustomSauceArray) {
+            if (item === true) {
+                sauceSelectionState = true
+            }
+        }
+        if (sauceSelectionState === false) {
+            alert('Please select a custom pizza sauce.')
+            return
+        }
+    } else {
+        // No fields have been populated whatsoever
+        alert('Please fill out the form before adding to cart.')
+        return
+    }
 
     // Create the object to be passed to sessionStorage
     let cartAddObj = undefined;
-    if (submissionType == 'specialty') {
+    if (submissionType === 'specialty') {
         // This is a specialty pizza
         cartAddObj = {
             itemType: submissionType,
@@ -89,4 +134,14 @@ addToCartFormNode.addEventListener('submit', function(e) {
     console.log(cartObj)
 })
 
-
+// Handle the empty cart button
+let addToCartButtonNode = document.getElementById('empty_cart');
+addToCartButtonNode.addEventListener('click', function(e) {
+    // Reset the papaGCart object in the sessionStorage to it's default state
+    let papaGCart = {
+        "objId": '5246673658',
+        "contentArray": []
+    }
+    sessionStorage.setItem('papaGCart', JSON.stringify(papaGCart));
+    alert("The cart has been reset.")
+})
--- js/order.js
+++ js/order.js
@@ -3,3 +3,4 @@ document.addEventListener('DOMContentLoaded',function() {
     document.getElementById('orderForm').addEventListener('submit',function(e){
         e.preventDefault();
         
+        
